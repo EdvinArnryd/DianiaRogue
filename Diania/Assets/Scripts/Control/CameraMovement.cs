c725cf6a7d382db55d2cda815157b546570d194c
Edvin <edvin.arn@hotmail.com>
2025-09-01 23:14:43 +0200

    Added movement

---
+++ Diania/Assets/Scripts/Control/CameraMovement.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] private float _speed = 1f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+            direction += Vector3.up;
+        if (Input.GetKey(KeyCode.S))
+            direction += Vector3.down;
+        if (Input.GetKey(KeyCode.A))
+            direction += Vector3.left;
+        if (Input.GetKey(KeyCode.D))
+            direction += Vector3.right;
+
+        if (direction != Vector3.zero) // prevent NaN when normalizing zero vector
+        {
+            direction.Normalize(); // makes diagonal magnitude = 1
+            transform.position += direction * _speed * Time.deltaTime;
+        }
+    }
+}
