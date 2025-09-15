e968b54fd3725c3071249005e6b6a2011153da56
Edvin <edvin.arn@hotmail.com>
2025-09-15 18:52:03 +0200

    Added moving player and idle

--- Diania/Assets/Scripts/Control/CameraMovement.cs
+++ Diania/Assets/Scripts/Control/CameraMovement.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
@@ -5,6 +6,10 @@ public class CameraMovement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     [SerializeField] private float _speed = 1f;
+    [SerializeField] private Transform _playerTransform;
+
+    [SerializeField] private GameObject _idleSprite;
+    [SerializeField] private GameObject _gif;
 
     private Player _player;
     void Start()
@@ -19,13 +24,33 @@ public class CameraMovement : MonoBehaviour
         Vector3 direction = Vector3.zero;
 
         if (Input.GetKey(KeyCode.W))
+        {
             direction += Vector3.up;
+        }
         if (Input.GetKey(KeyCode.S))
+        {
             direction += Vector3.down;
+        }
         if (Input.GetKey(KeyCode.A))
+        {
             direction += Vector3.left;
+            _playerTransform.rotation = Quaternion.Euler(0, 180, 0);
+        }
         if (Input.GetKey(KeyCode.D))
+        {
             direction += Vector3.right;
+            _playerTransform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        if(direction.sqrMagnitude < 1)
+        {
+            _idleSprite.GameObject().SetActive(true);
+            _gif.GameObject().SetActive(false);
+        }
+        else
+        {
+            _idleSprite.GameObject().SetActive(false);
+            _gif.GameObject().SetActive(true);
+        }
 
         if (direction != Vector3.zero) // prevent NaN when normalizing zero vector
         {
