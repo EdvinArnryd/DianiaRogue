39cb8abc0fbcb495dc200bcc3ffd30d0dcab2e32
Edvin <edvin.arn@hotmail.com>
2025-09-22 23:35:28 +0200

    Added pause logic

---
+++ Diania/Assets/Scripts/Managers/PauseManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    private bool _isPaused = false;
+
+    [SerializeField] private GameObject _pausePanel;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    private void TogglePause()
+    {
+        _isPaused = !_isPaused;
+
+        if (_isPaused)
+        {
+            Time.timeScale = 0f;
+            _pausePanel.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            _pausePanel.SetActive(false);
+        }
+    }
+}
