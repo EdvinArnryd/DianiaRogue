70c316689b99ae08e86a4d1ebc0241b880ddf01d
unknown <edvin.arn@hotmail.com>
2025-09-27 20:51:00 +0200

    worked more on pause manager

--- Diania/Assets/Scripts/Managers/PauseManager.cs
+++ Diania/Assets/Scripts/Managers/PauseManager.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,12 +14,17 @@ public class PauseManager : MonoBehaviour
 {
     private bool _isPaused = false;
 
+    [Header("UI Elements")]
     [SerializeField] private GameObject _pausePanel;
     [SerializeField] private GameObject _pauseMenu;
     [SerializeField] private GameObject _deathMenu;
     [SerializeField] private GameObject _levelUpMenu;
+    [SerializeField] private TMP_Text _text;
+    
+    [Header("Class References")]
     [SerializeField] private Player _player;
     [SerializeField] private LevelSystem _levelSystem;
+    
     private Image _pausePanelImage;
 
     private MenuState _currentMenuState = MenuState.None;
@@ -83,14 +89,20 @@ public class PauseManager : MonoBehaviour
             case MenuState.EscapeMenu:
                 _pauseMenu.SetActive(true);
                 _pausePanelImage.color = new Color(100f / 255f, 100f / 255f, 100f / 255f, 200f / 255f);
+                _text.text = "Game Paused";
+                _text.color = Color.black;
                 break;
             case MenuState.DeathMenu:
                 _deathMenu.SetActive(true);
                 _pausePanelImage.color = new Color(126f / 255f, 35f / 255f, 35f / 255f, 200f / 255f);
+                _text.text = "You Died!";
+                _text.color = Color.red;
                 break;
             case MenuState.LevelUpMenu:
                 _levelUpMenu.SetActive(true);
                 _pausePanelImage.color = new Color(51f / 255f, 63f / 255f, 100f / 255f, 200f / 255f);
+                _text.text = "Level Up!";
+                _text.color = Color.blue;
                 break;
         }
     }
