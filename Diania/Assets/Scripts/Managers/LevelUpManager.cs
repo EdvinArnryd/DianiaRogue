b8f25423eec0240f8272a17f4854d7b5ea005057
unknown <edvin.arn@hotmail.com>
2025-09-28 15:45:12 +0200

    set up core logic for levelupmenu

---
+++ Diania/Assets/Scripts/Managers/LevelUpManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelUpManager : MonoBehaviour
+{
+    [SerializeField] private Button _button1;
+    [SerializeField] private Button _button2;
+    [SerializeField] private Button _button3;
+
+
+
+    private void SetButtonData()
+    {
+        
+    }
+
+    private void OnButtonPress()
+    {
+        
+    }
+}
