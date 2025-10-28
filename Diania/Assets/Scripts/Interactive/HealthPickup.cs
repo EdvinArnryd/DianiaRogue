3affb613ce85414494ca8e4014484bc7c87cef0c
Edvin <edvin.arn@hotmail.com>
2025-10-28 22:54:32 +0100

    added effect to heartpickup

--- Diania/Assets/Scripts/Interactive/HealthPickup.cs
+++ Diania/Assets/Scripts/Interactive/HealthPickup.cs
@@ -3,6 +3,13 @@ using UnityEngine;
 public class HealthPickup : MonoBehaviour
 {
     [SerializeField] private int _healthAmount = 70;
+    [SerializeField] private float _speed = 2;
+    [SerializeField] private float _height = 2;
+
+    private void Update()
+    {
+        transform.position += transform.up * Mathf.Cos(Time.time * _height) * Time.deltaTime * _speed;
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
